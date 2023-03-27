3384be6824e935e21c58bec4e2b27e440cb3cd4d
Tarun Mandhan <mandhantarun@gmail.com>
2023-03-27 16:29:12 +0530

    Now firebase app is connected to firebase account

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCk4W8cDAs2Z1vfUroMuMncHAxtdlnLX7Q",
+  authDomain: "fir-v9-practice1.firebaseapp.com",
+  projectId: "fir-v9-practice1",
+  storageBucket: "fir-v9-practice1.appspot.com",
+  messagingSenderId: "90951468357",
+  appId: "1:90951468357:web:1cfef13dfcccc4c221aa2c",
+};
+
+export const app = initializeApp(firebaseConfig);
