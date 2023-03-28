0608fcf9ccaa6de155bc6f705216a073ae8d66f2
Tarun Mandhan <mandhantarun@gmail.com>
2023-03-28 17:43:04 +0530

    import SignupPage & use it

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import React from "react";
 // import { getDatabase, ref, set } from "firebase/database";
-import { app } from "./firebase";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import SignupPage from "./pages/Signup";
 
 // const db = getDatabase(app); // this db is instance, interact with database .
-const auth = getAuth(app);
 const App = () => {
   // const putData = () => {
   //   set(ref(db, "users/tarun"), {
@@ -14,16 +12,13 @@ const App = () => {
   //   });
   // };
 
-  const signupUser = () => {
-    createUserWithEmailAndPassword(auth, "tarun5@gmail.com", "tarun@5").then(
-      (value) => console.log(value)
-    );
-  };
   return (
     <>
-      <h1>Hello Tarun</h1>
-      {/* <button onClick={putData}>Put Data</button> */}
-      <button onClick={signupUser}>Sign up</button>
+      <div className="app">
+        <h1>Hello Tarun</h1>
+        {/* <button onClick={putData}>Put Data</button> */}
+        <SignupPage />
+      </div>
     </>
   );
 };
