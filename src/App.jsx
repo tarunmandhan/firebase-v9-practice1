fae61338ceeb12bb155d491db8dae0d1d1ca98be
Tarun Mandhan <mandhantarun@gmail.com>
2023-03-28 15:01:50 +0530

    interact with database

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
 import React from "react";
+import { getdatabase } from "firebase/database";
+import { app } from "./firebase";
+
+
+const db = getdatabase(app);  // this db is instance, interact with database . 
 
 const App = () => {
   return (
