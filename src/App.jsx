a9941a227eebf7e349d3c5b903d120735b0f7750
Tarun Mandhan <mandhantarun@gmail.com>
2023-03-28 15:43:28 +0530

    import set & ref , and create method putdata & button

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import React from "react";
-import { getdatabase } from "firebase/database";
+import { getDatabase, ref, set } from "firebase/database";
 import { app } from "./firebase";
 
-
-const db = getdatabase(app);  // this db is instance, interact with database . 
+const db = getDatabase(app); // this db is instance, interact with database .
 
 const App = () => {
+  const putData = () => {
+    set(ref(db, "users/tarun"), {
+      id: 1,
+      name: "Tarun",
+      age: 34,
+    });
+  };
   return (
     <>
       <h1>Hello Tarun</h1>
+      <button onClick={putData}>Put Data</button>
     </>
   );
 };
